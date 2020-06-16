4168c392ccbb70248833869a84ab4f8ddc447d08
shinhonggyu <66147646+shinhonggyu@users.noreply.github.com>
2020-06-16 20:45:07 +0900

    JS.Clock Image

--- clock.js
+++ clock.js
@@ -1,3 +1,36 @@
+window.addEventListener("load", function () {
+  clockRun();
+});
+
+function clockRun() {
+  var d = new Date();
+
+  var s = d.getSeconds();
+  var s_angle = s * 6; // 1초가 6도
+  var s_angle_value = "rotate(" + s_angle + "deg)";
+  document.getElementById("second").style.transform = s_angle_value;
+
+  var m = d.getMinutes();
+  var m_angle = m * 6; // 1초가 6도
+  var m_angle_value = "rotate(" + m_angle + "deg)";
+  document.getElementById("minute").style.transform = m_angle_value;
+
+  var h = d.getHours();
+
+  if (h < 12) {
+    var h = h;
+  } else {
+    var h = h - 12;
+  }
+
+  var h_angle = h * 30 + (30 / 60) * m;
+  var h_angle_value = "rotate(" + h_angle + "deg)";
+  document.getElementById("hour").style.transform = h_angle_value;
+
+  setTimeout(clockRun, 1000);
+}
+
+// Nomad
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
 
